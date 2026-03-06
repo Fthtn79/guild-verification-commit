193ae5e5694817a3634d54ced069101db0ea7cf0
Fatih Tuna <fthtn4@gmail.com>
2026-03-06 03:55:18 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:54:11.634Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T00:55:18.204Z
+Commit: Fix CSS styling bug on mobile devices
