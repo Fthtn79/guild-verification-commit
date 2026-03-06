854bda27e15aaca7c631ea4ecdee07bf934194a0
Fatih Tuna <fthtn4@gmail.com>
2026-03-06 03:57:08 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T00:57:06.480Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T00:57:07.869Z
+Commit: Fix memory leak in event handlers
